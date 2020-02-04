0b870ec004ed87c1a4a8a7627a381af2a394cff2
MarionD <49981777+MarionDouet@users.noreply.github.com>
2020-02-04 07:17:06 +0100

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ const  mysql = require('mysql');
 const  connection = mysql.createConnection({
 host :  'localhost', // adresse du serveur
 user :  'root', // le nom d'utilisateur
-password :  'Tv36Lu24', // le mot de passe
+password :  '', // le mot de passe
 database :  'pokedex', // le nom de la base de données
 });
 module.exports = connection;
